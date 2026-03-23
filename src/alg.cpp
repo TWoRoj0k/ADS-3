99899249ae361d0728ddd0895806e96af8f70d2e
TWoRoj0k <madgamerpro331@gmail.com>
2026-03-23 23:41:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,52 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
-}
+    uint64_t maxVal = num;
 
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = 3 * num + 1;
+        }
+
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+
+    return maxVal;
+}
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int length = 1;
+
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = 3 * num + 1;
+        }
+        length++;
+    }
+
+    return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int bestNum = static_cast<unsigned int>(lbound);
+    unsigned int maxLength = collatzLen(lbound);
+
+    for (uint64_t num = lbound + 1; num <= rbound; ++num) {
+        unsigned int currentLength = collatzLen(num);
+
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+            bestNum = static_cast<unsigned int>(num);
+        }
+    }
+
+    *maxlen = maxLength;
+    return bestNum;
 }
